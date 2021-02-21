ed6ef910390f000111afe1db51f5c2ff7644742e
Natalia <natalia_alexandra@web.de>
2021-02-21 17:58:20 +0100

    static

--- django_first/settings.py
+++ django_first/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'django_first/static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'django_first/static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
